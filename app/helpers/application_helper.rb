73fcbb53bc22ba3a4f22d2ce83e891a3448c551f
Nguyen Viet Hung <nguyen.viet.hung@framgia.com>
2017-10-30 09:53:55 +0700

    re-design static-pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    default_title = t "title"
+    return default_title if page_title.strip.empty?
+    "#{page_title} | #{default_title}"
+  end
 end
